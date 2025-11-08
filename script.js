ccce880b44a052c3cf6f5de5bd8cd8065648d5e7
Abhishek Gaur <agaur0136@gmail.com>
2025-11-08 11:51:29 +0530

    task add functionality completed

---
+++ script.js
@@ -0,0 +1,86 @@
+const btnAdd = document.querySelector(".btn-add");
+const overlay = document.querySelector(".overlay");
+const xMark = document.querySelector(".fa-xmark");
+const btnSave = document.querySelector(".btn-save");
+const todoContainer = document.querySelector(".todo-container");
+
+btnAdd.addEventListener("click", () => {
+  overlay.style.top = "0";
+});
+
+xMark.addEventListener("click", () => {
+  overlay.style.top = "-100vh";
+});
+
+btnSave.addEventListener("click", () => {
+  overlay.style.top = "-100vh";
+});
+
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function saveTasksToLocalStorage() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function renderTask() {
+  todoContainer.innerHTML = "";
+  tasks.map((task) => {
+    
+    const borderColor =
+      task.priority === "high"
+        ? "border-red"
+        : task.priority === "medium"
+        ? "border-orange"
+        : "border-green";
+
+    const backgroundColor =
+      task.priority === "high"
+        ? "background-red"
+        : task.priority === "medium"
+        ? "background-orange"
+        : "background-green";
+
+    todoContainer.innerHTML += `<div class="todo ${borderColor}">
+          <div class="todo-content">
+            <input type="checkbox" />
+            <div>
+              <h2>${task.title}</h2>
+              <p>${task.description}</p>
+              <div class="date-priority-container">
+                <span class="${backgroundColor}">${task.priority} priority</span>
+                <span>Due: ${task.dueDate}</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="icon-container">
+            <i class="fa-solid fa-pen"></i>
+            <i class="fa-solid fa-trash" ></i>
+          </div>
+        </div>`;
+  });
+}
+
+function formSubmitHandler(e) {
+  e.preventDefault();
+
+  const title = document.getElementById("title").value;
+  const description = document.getElementById("description").value;
+  const dueDate = document.getElementById("dueDate").value;
+  const priority = document.getElementById("priority").value;
+
+  let task = {
+    id: Date.now(),
+    title,
+    description,
+    dueDate,
+    priority,
+  };
+
+  tasks.push(task);
+  saveTasksToLocalStorage();
+  renderTask();
+}
+renderTask();
+
+// localStorage.clear()
